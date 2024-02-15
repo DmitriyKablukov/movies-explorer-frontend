3487f2abb6a312a834bd4f6ce417efa5995233ba
DmitriyKablukov <dima-kablukov@ya.ru>
2024-02-15 16:56:11 +0300

    Исправление редиректа после onSignOut

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -112,7 +112,6 @@ export default function App() {
     localStorage.removeItem('filteredMoviesList');
     setFilteredMoviesList([]);
     setCurrentUser({});
-    navigate('/');
   }
 
   function handleEditProfile(values) {
